fec42ef362bc134c519e0a8b2fdaf4fc98c0965b
tdbf39 <tdbf39@mst.edu>
2018-10-20 17:51:53 -0500

    Add files via upload
    added test code for testing 20x4 LCD on its own tiva, and the main function to be placed into the BMS program

---
+++ Indicator_Function.ino
@@ -0,0 +1,37 @@
+//Voltage Indicator function
+//Timothy Bain
+
+//To be copy pasted into BMS?
+//Call this in main loop
+//packVoltage, cellVoltages in volts, packCurrent in amps
+//cellVoltages must be length NUM_CELLS (24)
+void voltagesToLCD(const float packVoltage, const float packCurrent, const float cellVoltages[])
+{
+
+  Serial7.begin(9600);
+  Serial.begin(9600); //Start serial communication at 9600
+  
+  Serial7.begin(9600); //Start communication with Serial7
+  
+  const int NUM_CELLS = 24; //this should be in header?
+  
+  //Send the clear command to the display - this returns the cursor to the beginning of the display
+  Serial7.write('|'); //Setting character
+  Serial7.write('-'); //Clear display
+
+  Serial7.print("Pack:");
+  Serial7.print(packVoltage, 1); //packVoltage from BMS, in V?
+  Serial7.print("V");
+  Serial7.print(" Cur:");
+  Serial7.print(packCurrent, 2); //packCurrent from BMS, in A?
+  Serial7.print("A");
+
+  for(int i = 0; i < NUM_CELLS; i++)
+  {
+    Serial7.print(i);
+    Serial7.print(":");
+    Serial7.print(cellVoltages[i+1], 1); //cellVoltage from BMS in V?  shows one decimal place
+    Serial7.print("V ");
+  }
+  //delay(1000); //probably don't want this in main loop
+}
